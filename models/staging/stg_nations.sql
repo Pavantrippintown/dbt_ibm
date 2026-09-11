df9d8983d231f4ecc763bc2d488ca8e2a109d052
Pavan Kumar P <pavan.trippintown@gmail.com>
2026-09-11 06:44:40 +0000

    new

--- models/staging/stg_nations.sql
+++ models/staging/stg_nations.sql
@@ -1,3 +1,10 @@
-select 
-n_nationkey nation_id,n_name name,n_regionkey region_id,n_comment n_regionkey
-from SOURCEDB.MKMALL.NATIONS
+with source_nations as (
+    select
+        n_nationkey as nation_id,  -- Make sure this is n_nationkey, NOT n_regionkey
+        n_name as nation_name,
+        n_regionkey as region_id,
+        n_comment as comment
+    from {{ source('src', 'nations') }}
+)
+
+select * from source_nations
--- models/v_stg_nations.sql
+++
@@ -1,1 +0,0 @@
-select * from SOURCEDB.MKMALL.NATIONS
